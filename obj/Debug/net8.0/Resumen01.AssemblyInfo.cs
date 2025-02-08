f57391a92bea2f0a51fc1886e503cec97cb12b2e
¨Smailyn <sceballov@gmail.com>
2025-02-07 21:37:32 -0400

    Descripción de los cambios realizados

--- obj/Debug/net8.0/Resumen01.AssemblyInfo.cs
+++ obj/Debug/net8.0/Resumen01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Resumen01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3d968112a527ecc98c8b4737a4832e8185e9093")]
 [assembly: System.Reflection.AssemblyProductAttribute("Resumen01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Resumen01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
